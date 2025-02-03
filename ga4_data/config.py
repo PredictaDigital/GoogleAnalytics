a90050eee9ee328a73e522b8b3ed17c225fa6b7c
Hamidreza Nademi <hmr.upwork@gmail.com>
2025-02-03 10:50:28 +0330

    Django REST API for GA4

---
+++ ga4_data/config.py
@@ -0,0 +1,25 @@
+
+
+# ga4_account_id	201778529			
+# ga4_account_name	Little Cheeks 
+# ga4_property_id	368891428
+# ga4_property_name Little Cheeks - GA4
+
+PROPERTY_ID = '368891428'   # Predicta account
+START_DATE = '30daysAgo'
+END_DATE = 'today'
+
+# OAuth 2.0 credentials file location
+CLIENT_SECRET_FILE_ROOT = 'Code/oauth/'
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+
+# SQL Server configuration
+SQL_SERVER = 'predicta.database.windows.net'
+SQL_DATABASE = 'Predicta_Portal_DB'
+SQL_USER = 'qb_Dev'
+SQL_PASSWORD = 'FsBTB8zXz0s6kPXM'
+
+
+# Path
+USET_PATH_OAUTH = 'ga4_data/oauth/client_secret_predicta.json'
+
