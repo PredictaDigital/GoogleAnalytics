d2b91874f6d6fad9e2fbf5c63bfda2c5b737af3a
Hamidreza Nademi <hmr.upwork@gmail.com>
2025-02-03 19:05:04 +0330

    Add files via upload

---
+++ Django_REST_GA4/ga4_data/config.py
@@ -0,0 +1,25 @@
+
+
+# ga4_account_id	201778529			
+# ga4_account_name	Little Cheeks 
+# ga4_property_id	368891428
+# ga4_property_name Little Cheeks - GA4
+
+PROPERTY_ID = '368891428'   # Predicta account
+START_DATE = '30daysAgo'
+END_DATE = 'today'
+
+# OAuth 2.0 credentials file location
+CLIENT_SECRET_FILE_ROOT = 'Code/oauth/'
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+
+# SQL Server configuration
+SQL_SERVER = ''
+SQL_DATABASE = ''
+SQL_USER = ''
+SQL_PASSWORD = ''
+
+
+# Path
+USET_PATH_OAUTH = 'ga4_data/oauth/client_secret_predicta.json'
+
